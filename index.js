892567d472f19b28cf6248341d5fd2d0e631d73e
Mayko Costa <maykopr@hotmail.com>
2024-05-06 16:16:15 -0300

    versão 1 concluída

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+
+const margin = { top: 40, left: 40, right: 40, bottom: 40 };
+const outWidth = 900;
+const outHeight = 600;
+const inWidth = outWidth - margin.left - margin.right;
+const inHeight = outHeight - margin.top - margin.bottom;
+
+
 // definição e tratamento dos dados
 d3.json(
     "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
 ).then((data) => {
     const years = data.data.map((item) => item[0].substring(0, 4));
-    const yearDate = data.data.map(item => new Date(item[0]));
+    const yearsDate = data.data.map(item => new Date(item[0]));
     const GDP = data.data.map((item) => item[1]);
-
-    const margin = { top: 30, left: 40, right: 30, bottom: 30 };
-    const outWidth = 700;
-    const outHeight = 500;
-    const innerWidth = outWidth - margin.left - margin.right;
-    const innerHeight = outHeight - margin.top - margin.bottom;
-
 
     //defininção do container
     const container = d3
@@ -20,29 +21,38 @@ d3.json(
         .attr("width", outWidth)
         .attr("height", outHeight);
 
+
+    container
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('x', -300)
+        .attr('y', 80)
+        .text('Gross Domestic Product');
+
+
     // definição dos eixos e escalas
     const yLinearScale = d3
         .scaleLinear()
         .domain([0, d3.max(GDP)])
-        .range([0, innerHeight]);
+        .range([0, inHeight]);
 
     const scaledGDP = GDP.map((item) => yLinearScale(item));
-
 
     const xScale = d3
         .scaleTime()
         .domain([
-            new Date(d3.min(years)),
-            new Date((parseFloat(d3.max(years)) + 1).toString())
+            d3.min(yearsDate),
+            d3.max(yearsDate)
         ])
-        .range([0, innerWidth]);
+        .range([0, inWidth]);
 
     const xAxis = d3.axisBottom(xScale);
 
     const yAxisScale = d3
         .scaleLinear()
         .domain([0, d3.max(GDP)])
-        .rangeRound([innerHeight, 0]);
+        .rangeRound([inHeight, 0]);
+
     const yAxis = d3.axisLeft(yAxisScale);
 
 
@@ -85,7 +95,7 @@ d3.json(
     svg
         .attr("width", barWidth)
         .attr("height", (d) => d)
-        .attr("x", (d, i) => xScale(yearDate[i]))
+        .attr("x", (d, i) => xScale(yearsDate[i]))
         .attr("y", (d) => outHeight - margin.bottom - d)
         .attr("class", "bar")
         .attr("data-date", (d, i) => data.data[i][0])
@@ -94,32 +104,32 @@ d3.json(
         .attr("transform", `translate(${margin.left}, 0)`)
         .on("mouseover", function (event, d) {
             const i = this.getAttribute("index");
-            // edita dinamincamente a aparência do card de info
 
             overlay
                 .transition()
                 .duration(0)
+                .style("opacity", 0.9)
                 .style("height", d + "px")
                 .style("width", barWidth + "px")
-                .style("opacity", 0.9)
-                .style("left", xScale(yearDate[i]) + "px")
+                .style("left", xScale(yearsDate[i]) + "px")
                 .style("top", outHeight - margin.bottom - d + "px")
                 .style("transform", `translateX(${margin.left}px)`);
-            tooltip.transition().duration(200).style("opacity", 0.9);
+            tooltip.transition().duration(0).style('opacity', 0.9);
             tooltip
-                .html(
+                .text(
                     `${years[i]}
                     $${GDP[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,")}
-                    Billion`
+                    Billion
+                    `
                 )
                 .attr("data-date", data.data[i][0])
-                .style("left", xScale(yearDate[i]) + "px")
+                .style("left", barWidth * i + "px")
                 .style("top", outHeight / 2 + "px")
-                .style("transform", `translateX(${margin.left}px)`);
+                .style("transform", `translateX(80px)`)
+                .style("opacity", "0.9");
         })
-        .on("mouseout", () => {
-            // desaparece o card
-            tooltip.transition().duration(200).style("opacity", 0);
-            overlay.transition().duration(200).style("opacity", 0);
+        .on("mouseout", function () {
+            tooltip.transition().duration(500).style('opacity', 0);
+            overlay.transition().duration(500).style('opacity', 0);
         });
 });
